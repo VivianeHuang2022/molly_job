aed0af1b4ae4457d45b1abf063bc37939c26d2a7
EOMZON <116791118+EOMZON@users.noreply.github.com>
2024-06-21 18:43:26 +0800

    20240621 fix coverletter Null value verification with redux init data

---
+++ src/redux/data/emptyCoverletter.js
@@ -0,0 +1,59 @@
+export const emptyCoverletterData = {
+  jobDataQP1: {
+    title: '',
+    company: '',
+    description: '',
+  },
+  jobDataQP2: {
+    title: '',
+    company: '',
+    description: '',
+  },
+  jobDataQP3: {},
+  jobDataQP4: {
+    background: '',
+    action: '',
+    result: '',
+  },
+  jobDataQP5: {
+    years: '',
+  },
+  stdDataQP1: {
+    drCountry: '',
+    drUni: '',
+    drDegree: '',
+    drMajor: '',
+  },
+  stdDataQP2: {
+    curCountry: '',
+    curCourses: '',
+    curDegree: '',
+    curMajor: '',
+    curUni: '',
+    drCountry: '',
+    drDegree: '',
+    drMajor: '',
+    drUni: '',
+  },
+  stdDataQP3: {
+    getAwards: '',
+    getCompetitions: '',
+    getConference: '',
+    getProject: '',
+    getSkills: '',
+    internCompany: '',
+    internRole: '',
+  },
+  stdDataQP4: {
+    careerOrGoal: '',
+  },
+  stdDataQP5: {
+    Address: '',
+    Birthday: '',
+    Email: '',
+    FirstName: '',
+    Nationality: '',
+    Surname: '',
+    Tel: '',
+  },
+};
