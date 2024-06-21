97eba01b797292a718325cdfd9f94a9ba3c27365
EOMZON <116791118+EOMZON@users.noreply.github.com>
2024-06-21 18:02:55 +0800

    20240621 test ok

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,32 @@
-// @ts-check
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.goto('http://localhost:3000/#/start');
+  await page.getByText('Studying abroadGeneration of').click();
+  await page.getByRole('button', { name: 'Cover Letter' }).click();
+  await page.getByPlaceholder('Email').click();
+  await page.getByPlaceholder('Email').fill('zonlily@outlook.com');
+  await page.getByPlaceholder('Password').click();
+  await page.getByPlaceholder('Password').fill('123456');
+  await page.getByRole('button', { name: 'Log in' }).click();
+  await page.getByPlaceholder('Germany').click();
+  await page.getByPlaceholder('Germany').fill('United States');
+  await page.getByPlaceholder('TU Berlin').click();
+  await page.getByPlaceholder('TU Berlin').fill('Harvard University');
+  await page.getByPlaceholder('Master').click();
+  await page.getByPlaceholder('Master').fill("Bachelor's Degree");
+  await page.getByPlaceholder('Computer Science').click();
+  await page.getByPlaceholder('Computer Science').fill('Computer Science');
+  await page.getByRole('button', { name: 'Next' }).click();
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  await page
+    .getByText(
+      "I am enthusiastic about pursuing my [ Bachelor's Degree ] degree at [ Harvard"
+    )
+    .fill(
+      "I am enthusiastic about pursuing my [ Bachelor's Degree ] degree at [ Harvard University ] in [ United States ] due to its distinguished reputation in [ Computer Science].I hold a [ high school, bachelor or master ] degree in[Data Science ] from [shanghai uni] in[ the country you studied ]. where I gained a solid foundation in my major and developed a strong background in [ Data Science ] . Moreover, my coursework in [ Machine Learning, Data Analysis ]  have deepened my interest in this field "
+    );
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await page.getByRole('button', { name: 'Next' }).click();
 });
